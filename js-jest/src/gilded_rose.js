206529795aa7da256204bd4138dac4bc21719908
Fiona Chiang <fiona.f.chiang@gmail.com>
2023-10-16 21:24:17 -0700

    Refactored tests
    TODO: refactor updateQuality, refactor configured items

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -15,7 +15,16 @@ class Shop {
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
         if (this.items[i].quality > 0) {
           if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1;
+            if (this.items[i].quality > 50) {
+              this.items[i].quality = 50;
+              this.items[i].quality--;
+            } else {
+              if (this.items[i].name === "Conjured") {
+                this.items[i].quality = this.items[i].quality - 2;
+              } else {
+                this.items[i].quality = this.items[i].quality - 1;
+              }
+            }
           }
         }
       } else {
@@ -36,7 +45,7 @@ class Shop {
         }
       }
       if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
+          this.items[i].sellIn = this.items[i].sellIn - 1;
       }
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name != 'Aged Brie') {
@@ -60,6 +69,31 @@ class Shop {
     return this.items;
   }
 }
+
+// Pretty simple, right? Well this is where it gets interesting:
+
+// 	- Once the sell by date has passed, Quality degrades twice as fast
+// 	- The Quality of an item is never negative
+// 	- "Aged Brie" actually increases in Quality the older it gets
+// 	- The Quality of an item is never more than 50
+// 	- "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
+// 	- "Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
+// 	Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but
+// 	Quality drops to 0 after the concert
+
+// We have recently signed a supplier of conjured items. This requires an update to our system:
+
+// 	- "Conjured" items degrade in Quality twice as fast as normal items
+
+// Feel free to make any changes to the UpdateQuality method and add any new code as long as everything
+// still works correctly. However, do not alter the Item class or Items property as those belong to the
+// goblin in the corner who will insta-rage and one-shot you as he doesn't believe in shared code
+// ownership (you can make the UpdateQuality method and Items property static if you like, we'll cover
+// for you).
+
+// Just for clarification, an item can never have its Quality increase above 50, however "Sulfuras" is a
+// legendary item and as such its Quality is 80 and it never alters.
+
 
 module.exports = {
   Item,
--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -4,6 +4,101 @@ describe("Gilded Rose", function() {
   it("should foo", function() {
     const gildedRose = new Shop([new Item("foo", 0, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
+    expect(items[0].name).toBe("foo");
+    expect(items[0].sellIn).toBe(-1);
+    expect(items[0].quality).toBe(0);
+  });
+
+  it ("should degrade twice as fast past sell by date", function() {
+    const gildedRose = new Shop([new Item("Quality", -1, 10)]);
+    const items = gildedRose.updateQuality();
+    gildedRose.updateQuality();
+    expect(items[0].quality).toBe(6);
+  });
+
+  it ("should not have a negative quality", function() {
+    const gildedRose = new Shop([new Item("Villager", -1, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(0);
+  })
+
+  it ("Aged Brie actually increases in Quality the older it gets", function() {
+    const gildedRose = new Shop([new Item("Aged Brie", 5, 10)]);
+    const items = gildedRose.updateQuality();
+    for (let i = 0; i < 10; i++) {
+      gildedRose.updateQuality();
+    }
+
+    expect(items[0].quality).toBe(27);
+  })
+
+  it ("The Quality of an item is never more than 50", function() {
+    const gildedRose = new Shop([new Item("FrostMourne", 50, 60)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(49)
+  })
+
+  it ("Sulfuras, being a legendary item, never has to be sold or decreases in Quality", function() {
+    const gildedRose = new Shop([new Item("Sulfuras, Hand of Ragnaros", 5, 80)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(80);
+  })
+
+  it ("Sulfuras doesn't decrease in quality even after 1000 days", function() {
+    const gildedRose = new Shop([new Item("Sulfuras, Hand of Ragnaros", 0, 80)]);
+    const items = gildedRose.updateQuality();
+    for (let i = 0; i < 1000; i++) {
+      gildedRose.updateQuality();
+    }
+    expect(items[0].quality).toBe(80);
+  })
+
+  it ("Backstage passes, like aged brie, increases in Quality as its SellIn value approaches", function() {
+    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 5, 10)]);
+    const items = gildedRose.updateQuality();
+
+    for(let i = 0; i < 4; i++) {
+      gildedRose.updateQuality();
+    }
+    expect(items[0].quality).toBe(25);
+  })
+
+  it ("Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but Quality drops to 0 after the concert", function() {
+    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 10, 10)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(12);
+
+    for (let i = 0; i < 6; i++) {
+      gildedRose.updateQuality();
+    }
+    expect(items[0].quality).toBe(26);
+
+    for (let i = 0; i < 4; i++) {
+      gildedRose.updateQuality();
+    }
+    expect(items[0].quality).toBe(0);
+  });
+
+  // it ("should return true if Conjured item", function() {
+  //   const gildedRose = new Shop([new Item("Conjured Imp", 2, 3)]);
+  //   const items = gildedRose.updateQuality();
+  //   console.log(items)
+  //   var PATTERN = /conjured/,
+  //   filtered = items.filter(function (str) { return str.includes(PATTERN); });
+  //   console.log(filtered)
+  // });
+
+  it ("should return true if Conjured item", function() {
+    const gildedRose = new Shop([new Item("Conjured", 2, 3)]);
+    const items = gildedRose.updateQuality();
+
+    items[0].name === "Conjured" ? true : false; 
+  });
+
+  it ("should decrease twice as fast if Conjured item", function() {
+    const gildedRose = new Shop([new Item("Conjured", 2, 10)]);
+    const items = gildedRose.updateQuality();
+
+    expect(items[0].quality).toBe(8);
   });
 });
